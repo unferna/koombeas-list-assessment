4d889a3dbfa8b0d1dcdcfeec7fed6ead08347be7
Fernando Florez <arrobaunferna@gmail.com>
2023-05-12 14:19:44 -0500

    [Chore] UI Changed

--- KoombeaTest/Home/Cells/UserInfoTableViewCell.swift
+++ KoombeaTest/Home/Cells/UserInfoTableViewCell.swift
@@ -9,14 +9,26 @@ import UIKit
 import Kingfisher
 
 class UserInfoTableViewCell: UITableViewCell {
+    @IBOutlet private weak var containerView: UIView!
     @IBOutlet private weak var userImage: UIImageView!
-    
     @IBOutlet private weak var nameLabel: UILabel!
     @IBOutlet private weak var usernameLabel: UILabel!
     @IBOutlet private weak var emailLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        containerView.layer.shadowColor = UIColor.black.cgColor
+        containerView.layer.shadowOffset = .zero
+        containerView.layer.shadowOpacity = 0.3
+        containerView.layer.shadowRadius = 10
+        containerView.layer.cornerRadius = 8
+        containerView.layer.shouldRasterize = true
+        
+        userImage.layer.cornerRadius = 8
     }
     
     func configureCell(with user: AppUser) {
